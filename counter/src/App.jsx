35972e57e79ba3b8aec46a05ac6383a8b1b295c1
FatimaIrshad <bibimemoona2017@gmail.com>
2024-01-10 10:40:34 +0500

    Updated App.jsx

--- counter/src/App.jsx
+++ counter/src/App.jsx
@@ -9,7 +9,6 @@ function App() {
   let deletecounter = () => {setCount(count -1)}
   return (
     <>
-      {/* <CustomButton count={count} setCount={setCount}></CustomButton> */}
       <button onClick={addcounter}>Add Counter {count < 10? count : count = 10}</button>
       <br /> <br />
       <button onClick={deletecounter}>Delete Counter {count > 0 ? count : count = 0}</button>
@@ -17,14 +16,5 @@ function App() {
   )
 }
 
-// function CustomButton(props){
- 
-//   function count1(){
-//     props.setCount(props.count + 1)
-//   }
-//   return <button onClick={count1}>
-//       Counter {props.count}
-//     </button>
-  
-// }
+
 export default App
