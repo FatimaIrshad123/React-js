be1a1bffed3d1ca46b8ff45ef626e8609f35f6f6
FatimaIrshad <bibimemoona2017@gmail.com>
2024-01-13 10:47:33 +0500

    Added tailwind and props practice

---
+++ tailwind-Props/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+
+//import './App.css'
+import Card from './components/Card'
+
+function App() {
+  
+  return (
+    <div>
+      
+       <Card username = "Fatima"></Card>
+      <Card></Card>
+      <Card post='Staff Engineer, Algolia'></Card> 
+    </div>
+  )
+}
+
+export default App
---
+++ tailwind-Props/src/components/Card.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+export default function Card({username = 'Javaria',post = 'Not Assigned yet'}){
+    
+    return(
+     <div>
+        <blockquote>
+       <p className="text-lg font-medium">
+        “Tailwind CSS is the only framework that I've seen scale
+        on large teams. It’s easy to customize, adapts to any design,
+        and the build size is tiny.”
+      </p></blockquote>
+    <figcaption className="font-medium">
+      <div className="text-sky-500 dark:text-sky-400">
+        {/* {props.username || "Fatima"} */}
+        {username}
+      </div>
+      <div className="text-slate-700 dark:text-slate-500">
+        {post}
+      </div>
+    </figcaption>
+  
+    
+    </div>
+    )
+}
