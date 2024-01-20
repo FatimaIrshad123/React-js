db9fc81bda2a16757bf44f99ad53e659d26ad13f
FatimaIrshad <bibimemoona2017@gmail.com>
2024-01-20 10:02:58 +0500

    Added usememo-hook practice

---
+++ useMemo-hook/src/App.jsx
@@ -0,0 +1,35 @@
+import { useState,useMemo } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+  const [value,setValue] = useState()
+  //memonization means remembering some output given an input and not computing it again
+  
+  function Counter(){
+    setCount(count + 1)
+  }
+
+  let sum = useMemo(() => {
+    let sum = 0;
+    for (let i = 1; i<= value; i++){
+      sum = sum + i
+    }
+    return sum
+  },[value])
+  
+  // let sum = 0;
+  //   for (let i = 1; i<= value; i++){
+  //     sum = sum + i
+  //     console.log(sum)
+  //   }
+  return (
+    <div>
+      <input type='Number' onChange={(e) => {setValue(e.target.value)}}></input>
+      <button onClick={Counter}>Count : {count}</button>
+      <div>The sum of {value} is {sum}</div>
+    </div>
+  )
+}
+
+export default App
