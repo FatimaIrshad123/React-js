33582a3e12af22f8898997777cb58c230a453e37
FatimaIrshad <bibimemoona2017@gmail.com>
2024-02-11 14:01:46 +0500

    Added data

---
+++ Recoil/atom-family/src/Todo.js
@@ -0,0 +1,15 @@
+export const Todo = [{
+    id : 1,
+    title : 'todo1',
+    description : 'Todo1'
+},
+{
+    id : 2,
+    title : 'todo2',
+    description : 'Todo2'
+},
+{
+    id : 3,
+    title : 'todo3',
+    description : 'Todo3'
+}]
