590e6d57e36a2f3a00b48bcb0c7a2690ccbea078
FatimaIrshad <bibimemoona2017@gmail.com>
2024-02-27 12:06:32 +0500

    Added atom file

---
+++ Recoil/selector-family/src/atom.js
@@ -0,0 +1,21 @@
+import axios from "axios";
+
+import { atom, atomFamily, selectorFamily } from "recoil";
+import { todo } from "./Todo";
+
+export const TodoAtomFamily = atomFamily({
+    key : 'TodoAtom',
+    default : selectorFamily({
+        key : 'TodoSelectorfamily',
+        get : (id) => async({get}) => {
+            await new Promise( r => (setTimeout(r,2000)))
+            
+            const res = await axios.get(`https://sum-server.100xdevs.com/todo?id=${id}`)
+            return res.data.todo
+        }
+    })
+    // id => {
+    //     return todo.find(x => x.id === id)
+    // }
+    
+})
